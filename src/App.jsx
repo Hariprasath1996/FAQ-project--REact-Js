ad2129eb784721144ad7a90d15f2382835c940e5
HariPrasath1996 <hasathphy96@gmail.com>
2024-06-11 11:57:29 +0530

    create elements and styles , now  add some data manually

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,41 @@
+import { useState } from "react"
+
+const FaqItem = () => {
+  const[show,setShow] = useState(false);
+  const toggleShowAnswer= ()=>{
+    setShow(!show)
+  }
+  return (
+    <div className={`FaqItem-container ${show ? "active" : ""}`}>
+      <div className="que-box" onClick={toggleShowAnswer}>
+          Sample Question ?
+      </div>
+      <div className="Ans-Container">
+        <div className="Ans">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis vitae quisquam aut quas odit quae quo corrupti laudantium, ad impedit.
+        </div>
+      </div>
+    </div>
+  )
+}
+const Faq = () => {
+  return (
+    <div className="Faq-container">
+      <h1>FAQs</h1>
+      <FaqItem />
+      <FaqItem />
+      <FaqItem />
+      <FaqItem />
+      <FaqItem />
+    </div>
+  )
+}
 const App = () => {
   return (
     <>
+      <div className="container">
+        <Faq />
+      </div>
     </>
   );
 }
